7636591c2feee7d2595fd686af55626bd8fa5a2f
Shawn Cole <Cole9350@gmail.com>
2024-05-16 23:01:47 -0400

    Result type refactor

--- GHFollowers/Views/FollowerLisViewController.swift
+++ GHFollowers/Views/FollowerLisViewController.swift
@@ -16,14 +16,14 @@ class FollowerLisViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
-            guard let followers else {
-                if let error {
-                    self.presentGFAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
-                }
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+                print(followers.count)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
             }
-            print(followers)
         }
     }
     
