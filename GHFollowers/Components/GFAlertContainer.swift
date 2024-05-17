d30d0e0cfc632313648d7ca7d1c85c30bd3cb7b6
Shawn Cole <Cole9350@gmail.com>
2024-05-16 20:58:55 -0400

    Bug Fixes

---
+++ GHFollowers/Components/GFAlertContainer.swift
@@ -0,0 +1,20 @@
+//
+//  GFAlertContainer.swift
+//  GHFollowers
+//
+//  Created by Shawn Cole on 5/16/24.
+//
+
+import UIKit
+
+class GFAlertContainer: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,8 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Shawn Cole on 5/16/24.
+//
+
+import Foundation
